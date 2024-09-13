b3e69e48013c52186c76835c966d12bc4e4d9588
Leon <leonleberre67@gmail.com>
2024-09-13 09:44:39 +0200

    Initial Commit

---
+++ TheMeal/TheMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMealApp.swift
+//  TheMeal
+//
+//  Created by Leon Le Berre on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
