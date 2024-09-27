77926491f5ca235c4e3edd4c78fd1af2b869405d
Thomas <thomas01brsd@gmail.com>
2024-09-27 10:00:59 +0200

    Tests unitaires et tests UI

--- TheMealTests/TheMealTests.swift
+++ TheMealTests/TheMealTests.swift
@@ -6,31 +6,86 @@
 //
 
 import XCTest
+import Alamofire
+import OHHTTPStubs
+import OHHTTPStubsSwift
 @testable import TheMeal
 
 final class TheMealTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    var viewModel: MealListViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        viewModel = MealListViewModel()
+        
+        
+        
+        // Stub the network request
+        
+        stub(condition: isHost("www.themealdb.com")) { _ in
+            let stubData = """
+            {
+                "meals": [
+                    {
+                        "strMeal": "Sushi",
+                        "strMealThumb": "https://www.themealdb.com/images/media/meals/g046bb1560454026.jpg",
+                        "idMeal": "53065"
+                    },
+                    {
+                        "strMeal": "Tempura",
+                        "strMealThumb": "https://www.themealdb.com/images/media/meals/1525873040.jpg",
+                        "idMeal": "53060"
+                    }
+                ]
+            }
+            """.data(using: .utf8)!
+            return HTTPStubsResponse(data: stubData, statusCode: 200, headers: nil)
         }
     }
-
+    
+    override func tearDown() {
+        // Remove all stubs after test
+        HTTPStubs.removeAllStubs()
+        super.tearDown()
+    }
+    
+    func testFetchDataSuccess() {
+        // Arrange
+        let expectation = XCTestExpectation(description: "Meals data fetched successfully")
+        
+        // Act
+        viewModel.fetchData(meal_area: "Japanese")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // Assert
+            XCTAssertEqual(self.viewModel.list.count, 2, "The list should contain 2 meals")
+            XCTAssertEqual(self.viewModel.list[0].strMeal, "Sushi", "First meal should be Sushi")
+            XCTAssertEqual(self.viewModel.list[1].strMeal, "Tempura", "Second meal should be Tempura")
+            XCTAssertEqual(self.viewModel.list[0].idMeal, "53065", "ID of the first meal should be 53065")
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 2.0)
+    }
+    
+    func testFetchDataFailure() {
+        // Arrange
+        HTTPStubs.removeAllStubs()  // Remove existing stubs to simulate a failure
+        stub(condition: isHost("www.themealdb.com")) { _ in
+            return HTTPStubsResponse(error: NSError(domain: NSURLErrorDomain, code: 500, userInfo: nil))
+        }
+        
+        let expectation = XCTestExpectation(description: "Meal data fetch should fail")
+        
+        // Act
+        viewModel.fetchData(meal_area: "Japanese")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // Assert
+            XCTAssertEqual(self.viewModel.list.count, 0, "The list should be empty due to the failure")
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 2.0)
+    }
 }
